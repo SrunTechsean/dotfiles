384a0a381db456e994183354f9f205d7ca28378f
SrunTechsean <techseansrun@gmail.com>
2026-02-20 14:38:11 +0700

    feat: change the keybind
    change the keybind from <leader>n to <leader>bn

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 -- Key mapping for buffer switching
 -- Leader mappings (<leader>n, <leader>p, <leader>d)
-vim.keymap.set("n", "<leader>n", ":bnext<cr>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<leader>p", ":bprevious<cr>", { desc = "Previous Buffer" })
-vim.keymap.set("n", "<leader>d", ":bdelete<cr>", { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
 
 vim.keymap.set({ "n", "t" }, "<C-/>", function()
   Snacks.terminal.toggle(nil, { cwd = vim.fn.expand("%:p:h") })
