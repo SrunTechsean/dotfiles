1890342025ac913b41b706f898345e8922a51cd4
SrunTechsean <techseansrun@gmail.com>
2026-02-20 14:32:41 +0700

    feat: add toggle terminal keybind
    Open up the terminal in that file location

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 vim.keymap.set("n", "<leader>n", ":bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>p", ":bprevious<cr>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>d", ":bdelete<cr>", { desc = "Delete Buffer" })
+
+vim.keymap.set({ "n", "t" }, "<C-/>", function()
+  Snacks.terminal.toggle(nil, { cwd = vim.fn.expand("%:p:h") })
+end)
