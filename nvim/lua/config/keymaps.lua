44a9f5d21ff68e5e4ef318ad85202858aaeee75d
SrunTechsean <techseansrun@gmail.com>
2026-03-12 14:42:23 +0700

    feat: toggle terminal keymap change to C-t to works in tmux

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,11 @@ vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
 
-vim.keymap.set({ "n", "t" }, "<C-/>", function()
-  Snacks.terminal.toggle(nil, { cwd = vim.fn.expand("%:p:h") })
-end)
+local _terminal_cwd = nil
+vim.keymap.set({ "n", "t" }, "<C-t>", function()
+  if not _terminal_cwd then
+    local cwd = vim.fn.expand("%:p:h")
+    _terminal_cwd = vim.fn.isdirectory(cwd) == 1 and cwd or vim.fn.getcwd()
+  end
+  Snacks.terminal.toggle(nil, { cwd = _terminal_cwd })
+end, { desc = "Terminal (cwd)" })
