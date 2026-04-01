71c48e834739f6a64173595aeb30295f51393f49
SrunTechsean <techseansrun@gmail.com>
2026-04-01 11:30:07 +0700

    refactor(nvim): remove formating for HTML
    Use Biome formatter instead

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,22 +4,6 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       folds = { enabled = false },
-      servers = {
-        html = {
-          filetypes = { "html", "htmldjango", "jinja", "jinga2" },
-          settings = {
-            html = {
-              format = {
-                wrapLineLength = 160,
-                extraLiners = "",
-                indentInnerHtml = true,
-                templating = true,
-              },
-            },
-          },
-        },
-        emmet_language_server = { "html", "css", "javascript", "javascriptreact", "typescriptreact" },
-      },
     },
   },
 }
