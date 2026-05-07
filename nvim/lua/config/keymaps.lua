e4e113e2768424220d0e93a2c19508db696c0e2d
SrunTechsean <techseansrun@gmail.com>
2026-05-07 13:41:29 +0700

    feat(nvim): compile c++ quickly with <leader>r

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,3 +15,59 @@ vim.keymap.set({ "n", "t" }, "<C-t>", function()
     end
     Snacks.terminal.toggle(nil, { id = "file_term", cwd = cwd })
 end, { desc = "Terminal (Current File)" })
+
+-- Compile C++ quickly
+vim.keymap.set("n", "<leader>r", function()
+    vim.cmd("silent! w")
+
+    local ft = vim.bo.filetype
+    local file = vim.fn.shellescape(vim.fn.expand("%:p"))
+    local file_no_ext = vim.fn.shellescape(vim.fn.expand("%:p:r"))
+    local dir = vim.fn.shellescape(vim.fn.expand("%:p:h"))
+    local raw_dir = vim.fn.expand("%:p:h")
+
+    local function compile_and_run(compiler, ext)
+        if vim.fn.filereadable(raw_dir .. "/Makefile") == 1 then
+            return "cd " .. dir .. " && make"
+        end
+        local choice = vim.fn.confirm("Compile Mode:", "&1. Just this file\n&2. All *." .. ext .. " files", 1)
+        if choice == 1 then
+            return "cd " .. dir .. " && " .. compiler .. " " .. file .. " -o " .. file_no_ext .. " && " .. file_no_ext
+        elseif choice == 2 then
+            return "cd " .. dir .. " && " .. compiler .. " *." .. ext .. " -o " .. file_no_ext .. " && " .. file_no_ext
+        end
+        return nil
+    end
+
+    local runners = {
+        javascript = "node " .. file,
+        python = "python3 " .. file,
+        sh = "bash " .. file,
+        c = function()
+            return compile_and_run("gcc", "c")
+        end,
+        cpp = function()
+            return compile_and_run("g++", "cpp")
+        end,
+    }
+
+    local runner = runners[ft]
+    if not runner then
+        vim.notify("No run command configured for filetype: " .. ft, vim.log.levels.WARN)
+        return
+    end
+
+    local cmd
+    if type(runner) == "function" then
+        cmd = runner()
+    else
+        cmd = runner
+    end
+    if not cmd then
+        return
+    end
+
+    vim.cmd("botright 15new")
+    vim.fn.jobstart(cmd, { term = true })
+    vim.cmd("startinsert")
+end, { desc = "Run/Compile Current File" })
