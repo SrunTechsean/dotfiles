513e777a2771d2981e112a786d23550c780c076c
SrunTechsean <techseansrun@gmail.com>
2026-06-15 14:17:49 +0700

    feat(diabled): enable mini.ai
    to use stuff like cie ciq

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-mini/mini.ai", enabled = false },
-  { "nvim-mini/mini.hipatterns", enabled = false },
-  { "nvim-mini/mini.pairs", enabled = false },
+    { "nvim-mini/mini.hipatterns", enabled = false },
+    { "nvim-mini/mini.pairs", enabled = false },
 }
